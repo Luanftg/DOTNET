65a12132a3f19d800040ecfc7e44ceb690ca2863
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2022-12-16 10:03:21 -0300

    [funcionalidades]: adiciona lista local de clientes com as funções de cadastrar e listar

--- Program.cs
+++ Program.cs
@@ -1,8 +1,12 @@
-﻿internal class Program
-{
+﻿using System.Text.Json;
 
-    public static List<Produto> listaDeProduto = new List<Produto>();
-    private static void Main(string[] args)
+internal class Program
+{
+    const string CAMINHO_ARQUIVOS = "/Users/Luan Fonseca/Desktop/Codigo do Futuro/DOTNET/Imports";
+    public static string[]? arquivos = Directory.GetFiles(CAMINHO_ARQUIVOS);
+    public static List<Produto> listaDeProdutos = new List<Produto>();
+    public static List<Cliente> listaDeClientes = new List<Cliente>();
+    private static void Main()
     {
         Console.WriteLine("""
 
@@ -12,6 +16,7 @@
 
         """);
         var opcao = 0;
+        atualizaListaLocal();
         
         do {
             menu();
@@ -34,20 +39,81 @@
                     mostrarQuantidadeDaLista();
                     opcao =0;
                     break;
+                case "4":
+                    cadastrarCliente();
+                    opcao =0;
+                    break;
+                case "5":
+                    listarClientes();
+                    opcao =0;
+                    break;
                 default:
                     opcao = 0;
                 break;
             }
         } while(opcao >=0);
     }
-static void menu(){
+
+    private static void listarClientes()
+    {
+        foreach (Cliente client in listaDeClientes)
+        {   
+            Console.WriteLine(client);
+        }
+    }
+
+    private static void atualizaArquivosLocais() {
+        Program.arquivos = Directory.GetFiles(CAMINHO_ARQUIVOS);
+        foreach (var arq in arquivos)
+        {
+            Console.WriteLine(arq);
+        }
+    }
+
+    private static void atualizaListaLocal(){
+        foreach (string arq in arquivos!)
+        {
+            string jsonString = File.ReadAllText(arq);
+            Cliente cliente = JsonSerializer.Deserialize<Cliente>(jsonString)!;
+            bool contains = Program.listaDeClientes.Contains(cliente);
+            Console.WriteLine("Contains: " + contains.ToString());
+            if(contains == false)
+            {
+                Program.listaDeClientes.Add(cliente);
+            }
+        }
+    }
+
+    private static void cadastrarCliente()
+    {
+        Console.WriteLine("\nInforme o nome do cliente:\n");
+        string? nomeDoCliente = Console.ReadLine();
+        Console.WriteLine("\nInforme o telefone do cliente:\n");
+        string? telefone = Console.ReadLine();
+        Cliente cliente = new Cliente(nomeDoCliente!,telefone!);
+        string fileName = "client"+ (cliente.Id).ToString() + ".json";
+        string directoryName = "C:\\Users\\Luan Fonseca\\Desktop\\Codigo do Futuro\\DOTNET\\Imports\\" + fileName;
+        Console.WriteLine(directoryName);
+
+        
+        string jsonString = JsonSerializer.Serialize(cliente);
+        Console.WriteLine(cliente.ToString());
+        File.WriteAllText(directoryName, jsonString);
+        atualizaArquivosLocais();
+        atualizaListaLocal();
+    }
+
+    static void menu(){
     Console.WriteLine("""
+
         Digite a opção desejada:
         ------------------------------------------
         0 -> Sair
         1 -> Cadastrar Produto
         2 -> Listar Produtos
         3 -> Exibir número de produtos cadastrados
+        4 -> Cadastrar Cliente
+        5 -> Listar Clientes
         ------------------------------------------
         """);
     }
@@ -59,25 +125,25 @@ static void menu(){
         Console.WriteLine("\nInforme a quantidade do produto:\n");
         int? quantidade = Convert.ToInt32(Console.ReadLine());
         var produto = new Produto(nome, quantidade);
-        Program.listaDeProduto.Add(produto);
+        Program.listaDeProdutos.Add(produto);
         Console.WriteLine("\nProduto Cadastrado com sucesso!\n");
     }
     static void listar() {
 
         Console.WriteLine("\nLista de Produtos: \n");
-        for(int index =0;index<Program.listaDeProduto.Count();index++)
+        for(int index =0;index<Program.listaDeProdutos.Count();index++)
         {
-            Console.WriteLine(Program.listaDeProduto[index] + "\n");
+            Console.WriteLine(Program.listaDeProdutos[index] + "\n");
         }
     }
     static void mostrarQuantidadeDaLista() {
         int? soma = 0;
-        var quantidadeDaLista = Program.listaDeProduto.Count();
+        var quantidadeDaLista = Program.listaDeProdutos.Count();
         for(int index =0;index<quantidadeDaLista;index++)
         {
-            var produto = Program.listaDeProduto[index];
-            Console.WriteLine("\n" + produto.nome + " : " + produto.quantidade);
-            soma += produto.quantidade;
+            var produto = Program.listaDeProdutos[index];
+            Console.WriteLine("\n" + produto.Nome + " : " + produto.Quantidade);
+            soma += produto.Quantidade;
         }
         
         Console.WriteLine("\nQuantidade Atual de Produtos no estoque: " + soma + "\n");
@@ -87,19 +153,38 @@ static void menu(){
 
 public class Produto
 {
-   private int id;
-   public string? nome;
-   public int? quantidade;
+   private int Id;
+   public string? Nome;
+   public int? Quantidade;
 
     public Produto(string? nome, int? quantidade)
     {
-     this.nome = nome; 
-     this.quantidade = quantidade;
-     this.id = Program.listaDeProduto.Count+1; 
+     this.Nome = nome; 
+     this.Quantidade = quantidade;
+     this.Id = Program.listaDeProdutos.Count+1; 
     }
 
     public override string? ToString()
     {
-        return "id: " + this.id + "\nnome: " +  this.nome + "\nquantidade: " + this.quantidade ;
+        return "id: " + this.Id + "\nnome: " +  this.Nome + "\nquantidade: " + this.Quantidade ;
+    }
+}
+
+public class Cliente {
+
+    public int Id {get;}
+    public string? Nome {get;set;}
+    public string? Telefone {get;set;}
+
+    public Cliente(string nome, string telefone)
+    {
+        this.Nome = nome;
+        this.Telefone = telefone;
+        this.Id = Program.listaDeClientes.Count+1;
+    }
+
+     public override string? ToString()
+    {
+        return "\nnome: " +  this.Nome + "\ntelefone: " + this.Telefone ;
     }
 }
